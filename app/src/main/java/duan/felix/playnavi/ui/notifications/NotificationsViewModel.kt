b3c7c04e265f19939d7a463e78c0b03ba4a7d99f
tdounnyy <tdounnyy@126.com>
2019-11-04 15:33:03 +0800

    Initial android navigation playground

---
+++ app/src/main/java/duan/felix/playnavi/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package duan.felix.playnavi.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+  private val _text = MutableLiveData<String>().apply {
+    value = "This is notifications Fragment"
+  }
+  val text: LiveData<String> = _text
+}
